7941a99bfd6f027c378d27392223b5246efbdf52
JSDM <161076@gmail.com>
2020-03-16 12:34:19 +0100

    finnish fron-end

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
-console.log('Client side javascript file is loaded!')
+// This javascript runs in client browser (front-end), not in nodejs (backend)
+// console.log('Client side javascript file is loaded!');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+// messageOne.textContent = '';
+// messageTwo.textContent = '';
+
+weatherForm.addEventListener('submit', e => {
+  e.preventDefault();
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+  const location = search.value;
+  let url = 'http://localhost:3000/weather?address=' + location;
+  fetch(url).then(res => {
+    res.json().then(data => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
+  });
+});
